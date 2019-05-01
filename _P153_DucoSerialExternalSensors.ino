2c34fe6d4d500a7efcd35afcc2f98a231b32b30c
Arne Mauer <arne.mauer@gmail.com>
2019-05-01 22:11:09 +0200

    New plugin for reading external sensors finished
    With this separate plugin it is possible to read multiple external sensors.
    
    TODO: remove reading CO2 sensors from _P151_DucoSerialGateway.

--- _P153_DucoSerialExternalSensors.ino
+++ _P153_DucoSerialExternalSensors.ino
@@ -11,9 +11,9 @@
 #define PLUGIN_153
 #define PLUGIN_ID_153           153
 #define PLUGIN_NAME_153         "DUCO Serial External Sensors"
-#define PLUGIN_VALUENAME1_153   "SENSOR_TEMP"     // sensorinfo
-#define PLUGIN_VALUENAME2_153   "SENSOR_RH"       // sensorinfo
-#define PLUGIN_VALUENAME3_153   "SENSOR_CO2_PPM"  // sensorinfo
+#define PLUGIN_VALUENAME1_153   "Temperature"     // sensorinfo
+#define PLUGIN_VALUENAME2_153   "Relative_humidity"       // sensorinfo
+#define PLUGIN_VALUENAME3_153   "CO2_PPM"  // sensorinfo
 #define PLUGIN_READ_TIMEOUT_153   1200 // DUCOBOX askes "live" CO2 sensor info, so answer takes sometimes a second.
 #define PLUGIN_LOG_PREFIX_153   String("[P153] DUCO EXTERNAL SENSOR: ")
 
@@ -32,7 +32,7 @@ typedef enum {
     P153_CONFIG_IDX_SENSOR_RH = 1,
     P153_CONFIG_IDX_SENSOR_CO2_PPM = 2,
     P153_CONFIG_DEVICE_TYPE = 3,
-    
+    P153_CONFIG_NODE_ADDRESS = 4,
 } P153PluginConfigs;
 
 typedef enum {
@@ -47,6 +47,19 @@ typedef enum {
         - Actuatorprint
       */
 } P153DucoSensorDeviceTypes;
+
+typedef enum {
+    P153_DUCO_PARAMETER_TEMP = 73,
+    P153_DUCO_PARAMETER_CO2 = 74,
+    P153_DUCO_PARAMETER_RH = 75,
+      /* add more device:
+        - Sensorless Control valve
+        - CO2 Control valve
+        - Humidity Control valve
+        - window ventilator
+        - Actuatorprint
+      */
+} P153DucoParameters;
 
 boolean Plugin_153(byte function, struct EventStruct *event, String& string)
 {
@@ -96,24 +109,19 @@ boolean Plugin_153(byte function, struct EventStruct *event, String& string)
 
         addHtml(F("<TR><TD>Sensor type:<TD>"));
         byte choice = PCONFIG(P153_CONFIG_DEVICE_TYPE);
-
-
         addSelector(String(F("Plugin_153_DEVICE_TYPE")), 3, options, NULL, NULL, choice, true);
 
-        if(choice == P153_DUCO_DEVICE_CO2 || choice == P153_DUCO_DEVICE_RH){
-        addFormNote(F("For use with domoticz you can define an idx per value. If you use this you can ignore 'Send to Controller' below."));
-        addFormNumericBox(F("IDX Sensor temperature"), F("Plugin_153_IDX1"), PCONFIG(P153_CONFIG_IDX_SENSOR_TEMP), 0, 5000);
+        if(choice != P153_DUCO_DEVICE_NA){
+          addFormNumericBox(F("Sensor Node address"), F("Plugin_153_NODE_ADDRESS"), PCONFIG(P153_CONFIG_NODE_ADDRESS), 0, 5000);
 
+          addFormNote(F("For use with domoticz you can define an idx per value. If you use this you can ignore 'Send to Controller' below."));
+          addFormNumericBox(F("IDX Sensor temperature"), F("Plugin_153_IDX1"), PCONFIG(P153_CONFIG_IDX_SENSOR_TEMP), 0, 5000);
         }
         if(choice == P153_DUCO_DEVICE_CO2){
           addFormNumericBox(F("IDX Sensor CO2 PPM"), F("Plugin_153_IDX3"), PCONFIG(P153_CONFIG_IDX_SENSOR_CO2_PPM), 0, 5000);
         }else if(choice == P153_DUCO_DEVICE_RH){
           addFormNumericBox(F("IDX Sensor relative humidity"), F("Plugin_153_IDX2"), PCONFIG(P153_CONFIG_IDX_SENSOR_RH), 0, 5000);
         }
-
-
-
-
 
         success = true;
         break;
@@ -123,22 +131,41 @@ boolean Plugin_153(byte function, struct EventStruct *event, String& string)
       {
 
         PCONFIG(P153_CONFIG_DEVICE_TYPE) = getFormItemInt(F("Plugin_153_DEVICE_TYPE"));
-        PCONFIG(P153_CONFIG_IDX_SENSOR_TEMP) = getFormItemInt(F("Plugin_153_IDX1"));
-        PCONFIG(P153_CONFIG_IDX_SENSOR_RH) = getFormItemInt(F("Plugin_153_IDX2"));
-        PCONFIG(P153_CONFIG_IDX_SENSOR_CO2_PPM) = getFormItemInt(F("Plugin_153_IDX3"));
+
+        if(PCONFIG(P153_CONFIG_DEVICE_TYPE) =! P153_DUCO_DEVICE_NA){
+          PCONFIG(P153_CONFIG_NODE_ADDRESS) = getFormItemInt(F("Plugin_153_NODE_ADDRESS"));
+          PCONFIG(P153_CONFIG_IDX_SENSOR_TEMP) = getFormItemInt(F("Plugin_153_IDX1"));
+        }
+
+        if(PCONFIG(P153_CONFIG_DEVICE_TYPE) == P153_DUCO_DEVICE_CO2){
+          PCONFIG(P153_CONFIG_IDX_SENSOR_CO2_PPM) = getFormItemInt(F("Plugin_153_IDX3"));
+        } else if(PCONFIG(P153_CONFIG_DEVICE_TYPE) == P153_DUCO_DEVICE_RH){
+          PCONFIG(P153_CONFIG_IDX_SENSOR_RH) = getFormItemInt(F("Plugin_153_IDX2"));
+        }
+
         success = true;
         break;
       }
       
     case PLUGIN_INIT:{
-            LoadTaskSettings(event->TaskIndex);
+            LoadTaskSettings(event->TaskIndex);        
+
+            // maybe we can change the valuecount based on the selecter sensortype. But... this isnt possible because espeasy searches in Device for the pluginnumber (getDeviceIndex)
+            // so... if there are multiple plugins activated we only get de index of the first device.
+            /*
+            byte DeviceIndex = getDeviceIndex(Settings.TaskDeviceNumber[event->TaskIndex]);
+            char logBuf[30];
+            snprintf(logBuf, sizeof(logBuf), "taskindex: %u; deviceindex: %u", event->TaskIndex, DeviceIndex);
+            addLog(LOG_LEVEL_DEBUG, PLUGIN_LOG_PREFIX_153 + logBuf);
+            */
+
 
             Serial.begin(115200, SERIAL_8N1);
             Plugin_153_init = true;
 
-            p153_duco_data[P153_DATA_SENSOR_TEMP] = 0.0; // box temp
-            p153_duco_data[P153_DATA_SENSOR_RH] = 0.0; // box rh
-            p153_duco_data[P153_DATA_SENSOR_CO2_PPM] = 0; // box CO2 ppm
+            p153_duco_data[P153_DATA_SENSOR_TEMP] = NAN; // box temp
+            p153_duco_data[P153_DATA_SENSOR_RH] = NAN; // box rh
+            p153_duco_data[P153_DATA_SENSOR_CO2_PPM] = NAN; // box CO2 ppm
 
             success = true;
             break;
@@ -148,7 +175,15 @@ boolean Plugin_153(byte function, struct EventStruct *event, String& string)
       {
         if (Plugin_153_init){
                 addLog(LOG_LEVEL_DEBUG, PLUGIN_LOG_PREFIX_153 + "read external duco sensors");
-                readExternalSensors();
+
+                if(PCONFIG(P153_CONFIG_DEVICE_TYPE) == P153_DUCO_DEVICE_CO2){
+                  readExternalSensors(P153_DATA_SENSOR_TEMP, PCONFIG(P153_CONFIG_NODE_ADDRESS));
+                  readExternalSensors(P153_DATA_SENSOR_CO2_PPM, PCONFIG(P153_CONFIG_NODE_ADDRESS));
+                } else if(PCONFIG(P153_CONFIG_DEVICE_TYPE) == P153_DUCO_DEVICE_RH){
+                  readExternalSensors(P153_DATA_SENSOR_TEMP, PCONFIG(P153_CONFIG_NODE_ADDRESS));
+                  readExternalSensors(P153_DATA_SENSOR_RH, PCONFIG(P153_CONFIG_NODE_ADDRESS));
+                }
+                
 
                 if (UserVar[event->BaseVarIndex + P153_DATA_SENSOR_TEMP] != p153_duco_data[P153_DATA_SENSOR_TEMP]) {
                     UserVar[event->BaseVarIndex + P153_DATA_SENSOR_TEMP] = p153_duco_data[P153_DATA_SENSOR_TEMP];
@@ -173,28 +208,55 @@ boolean Plugin_153(byte function, struct EventStruct *event, String& string)
   return success;
 }
 
-void readExternalSensors(){
-    addLog(LOG_LEVEL_DEBUG, PLUGIN_LOG_PREFIX_153 + "Start readBoxSensors");
-    
+void readExternalSensors(uint8_t dataType, int nodeAddress){
+    char logBuf[34];
+
     /*
      * Read box sensor information; this could contain CO2, Temperature and
      * Relative Humidity values, depending on the installed box sensors.
      */
         // SEND COMMAND: nodeparaget <Node> 74
     char command[20] = ""; /* 17 bytes + max 2 byte nodenumber + \r\n */
-    snprintf_P(command, sizeof(command), "nodeparaget %d 74\r\n", Settings.TaskDevicePluginConfig[task_index][P151_CONFIG_CO2_NODE]);
-    int commandSendResult = DucoSerialSendCommand(PLUGIN_LOG_PREFIX_151, (byte*) command, strlen(command));
+    uint8_t parameter;
 
+    if(dataType == P153_DATA_SENSOR_CO2_PPM){
+      parameter = P153_DUCO_PARAMETER_CO2;
+      snprintf(logBuf, sizeof(logBuf), "NodeAddress: %u. Type: CO2", nodeAddress);
+    }else if(dataType == P153_DATA_SENSOR_TEMP){
+      parameter = P153_DUCO_PARAMETER_TEMP;
+      snprintf(logBuf, sizeof(logBuf), "NodeAddress: %u. Type: temp", nodeAddress);
+    }else if(dataType == P153_DATA_SENSOR_RH){
+      parameter = P153_DUCO_PARAMETER_RH;
+      snprintf(logBuf, sizeof(logBuf), "NodeAddress: %u. Type: RH", nodeAddress);
+    }else{
+      return;
+    }
+    
+          
+    /* Reset value to NAN; only update values we can correctly read */
+        p153_duco_data[dataType] = NAN;
+
+    addLog(LOG_LEVEL_DEBUG, PLUGIN_LOG_PREFIX_153 + "Start read external sensor. " + logBuf );
+
+    snprintf_P(command, sizeof(command), "nodeparaget %d %d\r\n", nodeAddress, parameter);
+    int commandSendResult = DucoSerialSendCommand(PLUGIN_LOG_PREFIX_153, command);
 
     if (commandSendResult) {
         if (DucoSerialReceiveData(PLUGIN_LOG_PREFIX_153, PLUGIN_READ_TIMEOUT_153, false)) {
-            if (DucoSerialCheckCommandInResponse(PLUGIN_LOG_PREFIX_153, (uint8_t*) "sensorinfo", false)) {
+            /* Expected response is command minus the \n */
+            command[strlen(command) - 1] = '\0';
+            if (DucoSerialCheckCommandInResponse(PLUGIN_LOG_PREFIX_153, command, false)) {
                 addLog(LOG_LEVEL_DEBUG, PLUGIN_LOG_PREFIX_153 + "Received correct response on sensorinfo");
                 /* Example output:
-                    [SENSOR] INFO
-                    CO2 :  627 [ppm] (0)
-                    RH : 3246 [.01%] (0)
-                    TEMP :  235 [.1°C] (0)
+                   > nodeparaget 2 73
+                    Get PARA 73 of NODE 2
+                    --> 216
+                    Done
+                    
+                  > nodeparaget 2 74
+                    Get PARA 74 of NODE 2
+                    --> 492
+                    Done
                  */
                 /* Loop over all lines; make sure string is nul-terminated */
                 if (duco_serial_bytes_read == 0 || duco_serial_bytes_read > sizeof(duco_serial_buf)) {
@@ -209,30 +271,27 @@ void readExternalSensors(){
                 unsigned int raw_value;
                 while (token != NULL) {
                     char logBuf[30];
+                    if (sscanf(token, "  --> %u", &raw_value) == 1) {
 
-                    if (sscanf(token, "  CO2 : %u", &raw_value) == 1) {
+                      if(dataType == P153_DATA_SENSOR_CO2_PPM){
                         unsigned int co2_ppm = raw_value; /* No conversion required */
                         p153_duco_data[P153_DATA_SENSOR_CO2_PPM] = co2_ppm;
-
                         snprintf(logBuf, sizeof(logBuf), "CO2 PPM: %u = %u PPM", raw_value, co2_ppm);
-                        addLog(LOG_LEVEL_DEBUG, PLUGIN_LOG_PREFIX_153 + logBuf);
-                    }
 
-                    if (sscanf(token, "  RH : %u", &raw_value) == 1) {
-                        float rh = (float) raw_value / 100.;
-                        p153_duco_data[P153_DATA_SENSOR_RH] = rh;
-
-                        snprintf(logBuf, sizeof(logBuf), "RH: %u = %.2f%%", raw_value, rh);
-                        addLog(LOG_LEVEL_DEBUG, PLUGIN_LOG_PREFIX_153 + logBuf);
-                    }
-
-                    if (sscanf(token, "  TEMP : %u", &raw_value) == 1) {
+                      }else if(dataType == P153_DATA_SENSOR_TEMP){
                         float temp = (float) raw_value / 10.;
                         p153_duco_data[P153_DATA_SENSOR_TEMP] = temp;
-
                         snprintf(logBuf, sizeof(logBuf), "TEMP: %u = %.1f°C", raw_value, temp);
-                        addLog(LOG_LEVEL_DEBUG, PLUGIN_LOG_PREFIX_153 + logBuf);
+
+                      }else if(dataType == P153_DATA_SENSOR_RH){
+                        float rh = (float) raw_value / 100.;
+                        p153_duco_data[P153_DATA_SENSOR_RH] = rh;
+                        snprintf(logBuf, sizeof(logBuf), "RH: %u = %.2f%%", raw_value, rh);
+
+                      }
+                      addLog(LOG_LEVEL_DEBUG, PLUGIN_LOG_PREFIX_153 + logBuf);
                     }
+
                     token = strtok(NULL, "\r");
                 }
             } else { 
@@ -241,3 +300,5 @@ void readExternalSensors(){
         }
     }
 }
+
+
